3be7a4d329fa56de4bb82edab46ef56203ab2735
Manivarma26 <112606407+Manivarma26@users.noreply.github.com>
2024-10-13 19:45:22 +0530

    Update main.tf

--- InfrastructureAsCode/modules/iot/main.tf
+++ InfrastructureAsCode/modules/iot/main.tf
@@ -1,1 +1,15 @@
+resource "aws_iot_thing" "this" {
+  name = "MyESP32"
+}
 
+resource "aws_iot_policy" "this" {
+  name   = "MyIoTPolicy"
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Action = ["iot:Connect", "iot:Publish", "iot:Subscribe", "iot:Receive"],
+      Resource = "*"
+    }]
+  })
+}
