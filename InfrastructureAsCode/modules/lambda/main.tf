f2fac18bca3c3f8f50c84acdf29a7387d4e138ed
Manivarma26 <112606407+Manivarma26@users.noreply.github.com>
2024-10-13 19:45:48 +0530

    Update main.tf

--- InfrastructureAsCode/modules/lambda/main.tf
+++ InfrastructureAsCode/modules/lambda/main.tf
@@ -1,1 +1,14 @@
+resource "aws_lambda_function" "this" {
+  function_name = "DataProcessor"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "lambda_function.lambda_handler"
+  runtime       = "python3.8"
+
+  s3_bucket = var.s3_bucket
+  s3_key    = "lambda.zip"  # Change as necessary
+
+  environment {
+    BUCKET_NAME = var.s3_bucket
+  }
+}
 
