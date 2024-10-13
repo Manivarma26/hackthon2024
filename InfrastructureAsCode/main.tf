7b155373a54a9dcf7bdb82d1cbae4dbf0716f41d
Manivarma26 <112606407+Manivarma26@users.noreply.github.com>
2024-10-13 19:43:38 +0530

    Update main.tf

--- InfrastructureAsCode/main.tf
+++ InfrastructureAsCode/main.tf
@@ -1,1 +1,28 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+
+  required_version = ">= 1.0"
+}
+
+provider "aws" {
+  region = "us-west-2" 
+}
+
+module "s3" {
+  source = "./modules/s3"
+}
+
+module "iot" {
+  source = "./modules/iot"
+}
+
+module "lambda" {
+  source = "./modules/lambda"
+  s3_bucket = module.s3.bucket_name
+}
 
