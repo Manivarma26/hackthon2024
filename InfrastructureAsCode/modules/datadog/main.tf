977396695da39b855f4d00cb858a9bc1a1476fb2
Manivarma26 <112606407+Manivarma26@users.noreply.github.com>
2024-10-15 21:50:49 +0530

    Update main.tf

--- InfrastructureAsCode/modules/datadog/main.tf
+++ InfrastructureAsCode/modules/datadog/main.tf
@@ -1,1 +1,15 @@
+resource "datadog_dashboard" "iot_dashboard" {
+  title        = "IoT Dashboard"
+  description  = "Dashboard for monitoring IoT metrics"
 
+  widget {
+    title = "IoT Data Over Time"
+    timeseries {
+      requests {
+        query = "avg:aws.iot.messages {*}"
+      }
+    }
+  }
+
+  // Add more widgets as needed
+}
