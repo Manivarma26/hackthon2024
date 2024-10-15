a2745fca2581fddd65f85acaa073d1802c949e9b
Manivarma26 <112606407+Manivarma26@users.noreply.github.com>
2024-10-15 18:42:50 +0530

    Update variables.tf

--- InfrastructureAsCode/modules/s3/variables.tf
+++ InfrastructureAsCode/modules/s3/variables.tf
@@ -1,4 +1,29 @@
-variable "name" {
+variable "bucket_name" {
+  description = "The name of the S3 bucket"
+  type        = string
 }
-variable "environment" {
+
+variable "aws_region" {
+  description = "The AWS region to create the bucket in"
+  type        = string
+  default     = "us-east-1"
 }
+
+variable "acl" {
+  description = "The canned ACL to apply to the bucket"
+  type        = string
+  default     = "private"
+}
+
+variable "versioning" {
+  description = "Enable versioning for the bucket"
+  type        = bool
+  default     = false
+}
+
+variable "tags" {
+  description = "A map of tags to assign to the bucket"
+  type        = map(string)
+  default     = {}
+}
+
