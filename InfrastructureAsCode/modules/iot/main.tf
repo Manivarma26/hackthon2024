ca1e1b71d227896afd278317a277c0582553fe0f
Manivarma26 <112606407+Manivarma26@users.noreply.github.com>
2024-10-15 19:25:47 +0530

    Update main.tf

--- InfrastructureAsCode/modules/iot/main.tf
+++ InfrastructureAsCode/modules/iot/main.tf
@@ -1,15 +1,30 @@
-resource "aws_iot_thing" "this" {
-  name = "MyESP32"
+resource "aws_iot_thing" "iot" {
+  name = var.thing_name
 }
 
-resource "aws_iot_policy" "this" {
-  name   = "MyIoTPolicy"
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Effect = "Allow",
-      Action = ["iot:Connect", "iot:Publish", "iot:Subscribe", "iot:Receive"],
-      Resource = "*"
-    }]
-  })
+resource "aws_iot_policy" "iot" {
+  name   = var.policy_name
+  policy = var.policy_document
 }
+
+resource "aws_iot_certificate" "iot" {
+  count = var.create_certificate ? 1 : 0
+
+  csr = var.certificate_csr
+  status = "ACTIVE"
+}
+
+resource "aws_iot_thing_principal_attachment" "iot" {
+  count = var.create_certificate ? 1 : 0
+
+  thing_name      = aws_iot_thing.iot.name
+  principal       = aws_iot_certificate.iot[0].arn
+}
+
+resource "aws_iot_policy_attachment" "iot" {
+  count = var.create_certificate ? 1 : 0
+
+  policy_name = aws_iot_policy.iot.name
+  target      = aws_iot_certificate.iot[0].arn
+}
+
