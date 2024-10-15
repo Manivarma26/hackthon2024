6aed430f45650e309ab10aa89fda10d9ca88c729
Manivarma26 <112606407+Manivarma26@users.noreply.github.com>
2024-10-15 18:56:38 +0530

    Create variables.tf

---
+++ InfrastructureAsCode/variables.tf
@@ -0,0 +1,15 @@
+variable "aws_access_key_id" {
+  description = "AWS access key ID"
+  type        = string
+}
+
+variable "aws_secret_access_key" {
+  description = "AWS secret access key"
+  type        = string
+}
+
+variable "aws_region" {
+  description = "The AWS region to create resources in"
+  type        = string
+  default     = "us-east-1"
+}
