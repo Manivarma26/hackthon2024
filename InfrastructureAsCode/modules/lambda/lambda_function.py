098a3c9b93582baf45a23e2af76b9ddfab2e0d7b
Manivarma26 <112606407+Manivarma26@users.noreply.github.com>
2024-10-15 20:57:04 +0530

    Update lambda_function.py

--- InfrastructureAsCode/modules/lambda/lambda_function.py
+++ InfrastructureAsCode/modules/lambda/lambda_function.py
@@ -1,1 +1,22 @@
+import json
+import os
+import requests
+
+def lambda_handler(event, context):
+    # Extract data from the IoT event
+    data = json.dumps(event)
+    
+    # Send to Datadog
+    datadog_api_key = os.environ['DD_API_KEY']
+    url = f"https://http-intake.logs.datadoghq.com/v1/input/{datadog_api_key}"
+    
+    headers = {
+        'Content-Type': 'application/json',
+    }
+    
+    response = requests.post(url, headers=headers, data=data)
+    return {
+        'statusCode': response.status_code,
+        'body': response.text
+    }
 
