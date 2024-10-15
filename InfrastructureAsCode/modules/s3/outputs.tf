58293ba04094e0bd2df8ed30440a04092998ba19
Manivarma26 <112606407+Manivarma26@users.noreply.github.com>
2024-10-15 18:42:20 +0530

    Update outputs.tf

--- InfrastructureAsCode/modules/s3/outputs.tf
+++ InfrastructureAsCode/modules/s3/outputs.tf
@@ -1,4 +1,10 @@
-output "s3_arn" {
-  value       = aws_s3_bucket.s3-bucket.arn
-  description = "The id of S3"
+output "bucket_id" {
+  description = "The ID of the S3 bucket"
+  value       = aws_s3_bucket.hackthon.id
 }
+
+output "bucket_arn" {
+  description = "The ARN of the S3 bucket"
+  value       = aws_s3_bucket.hackthon.arn
+}
+
