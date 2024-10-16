dd7894d74a9ff233eb7c742d86601668b31676ae
Manivarma26 <112606407+Manivarma26@users.noreply.github.com>
2024-10-16 14:14:08 +0530

    Update outputs.tf

--- InfrastructureAsCode/modules/iot/outputs.tf
+++ InfrastructureAsCode/modules/iot/outputs.tf
@@ -1,14 +1,14 @@
 output "thing_arn" {
   description = "The ARN of the IoT Thing"
-  value       = aws_iot_thing.this.arn
+  value       = aws_iot_thing.iot.arn
 }
 
 output "policy_arn" {
   description = "The ARN of the IoT Policy"
-  value       = aws_iot_policy.this.arn
+  value       = aws_iot_policy.iot.arn
 }
 
 output "certificate_arn" {
   description = "The ARN of the IoT Certificate"
-  value       = aws_iot_certificate.this[0].arn
+  value       = aws_iot_certificate.iot[0].arn
 }
