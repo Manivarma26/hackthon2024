7ba9e8329f40a84c2f9e21c371137faf72427828
Manivarma26 <112606407+Manivarma26@users.noreply.github.com>
2024-10-16 14:51:26 +0530

    Update outputs.tf

--- InfrastructureAsCode/modules/iot/outputs.tf
+++ InfrastructureAsCode/modules/iot/outputs.tf
@@ -12,3 +12,6 @@ output "certificate_arn" {
   description = "The ARN of the IoT Certificate"
   value       = aws_iot_certificate.iot[0].arn
 }
+output "certificate_arn" {
+  value = var.certificate_arn
+}
